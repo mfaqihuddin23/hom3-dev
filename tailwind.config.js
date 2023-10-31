f16558811997a399f7041c7f96e2f57d4d2d9707
unknown <mfaqihuddin23@gmail.com>
2023-10-31 14:45:30 +0700

    navbar prep

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1280px',
+      },
+    },
     extend: {},
   },
   plugins: [],

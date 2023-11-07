a39dd314d394c58624ecadc5e8bd8be2c2083871
Exzaraja <exzathena23@gmail.com>
2023-11-07 15:02:31 +0700

    how it works

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,15 @@ module.exports = {
         'prim-txt':"#252525",
         'second-txt':"#677487",
         'accent':"#D9D9D9",
+        'hvr':'#DFE1E7',
+        'subdued':'#818898',
+        'norm-txt':'#666D80',
+        'stabilo':'#D0F500',
         'grey':{
+          50:'#F8FAFB',
           200:'#F0F4FD',
           300:'#D4DBEA',
+          900:'#0D0D12',
         },
         'primary':{
           100:'#F2FFCC',
